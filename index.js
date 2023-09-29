d7c548b28ce2a638323e26bc5f28af5b2beb4ac2
Apurbo Roy <apurboroy7077@gmail.com>
2023-09-29 14:47:46 +0600

    Updated Port Variable in index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ console.log("Hello World");
 let app = require("./factory");
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your server is hosted at http://localhost:3000`);
+  console.log(`Your server is hosted at http://localhost:${port}`);
 });
